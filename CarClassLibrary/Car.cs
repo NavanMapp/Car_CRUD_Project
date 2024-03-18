5285198e3276b398149a7da6ea24aae0871c40df
Mapp_ <Mapp_@MAPPWORLDWIDE>
2024-03-18 19:27:10 +0200

    Add project files.

---
+++ CarClassLibrary/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarClassLibrary
+{
+    public class Car
+    {
+        public string Make { get; set; }
+        public string Model { get; set; }
+
+        public decimal Price{ get; set; }
+
+        public Car()
+        {
+            this.Make = Make;
+            this.Model = Model;
+            this.Price = Price;
+        }
+
+        public Car(string a, string b, decimal c)
+        {
+            Make = a;
+            Model = b;
+            Price = c;
+        }
+    }
+}
